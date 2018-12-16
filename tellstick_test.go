fc8b0e2bba6f2c430e19086811937d71d68fea6b
Joel Midstjärna <joel.midstjarna@gmail.com>
2018-12-16 14:42:00 +0100

    added more tests

--- tellstick_test.go
+++ tellstick_test.go
@@ -40,17 +40,74 @@ func TestGetDevices(t *testing.T) {
 	}
 }
 
-func TestCreateDevice(t *testing.T) {
+func createDeviceTest(t *testing.T, name string, protocol string, model string,
+	params map[string]string, supportOnOff bool, supportDim bool, supportLearn bool) {
+	// Create device
 	id, err := tl.NewDevice()
 	if err != nil {
 		t.Fatalf("Failed to create device. Reason: %s", err)
 	}
-	err = tl.SetName(id, "TestCreateDevice")
+
+	// Setup device
+	err = tl.SetName(id, name)
 	if err != nil {
 		t.Fatalf("Failed to set name on device %d. Reason: %s", id, err)
 	}
+	err = tl.SetProtocol(id, protocol)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	err = tl.SetModel(id, model)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	err = tl.SetParameters(id, params)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	// Check device
+	if tl.SupportsOnOff(id) != supportOnOff {
+		t.Fatalf("SupportsOnOff missmatch. Expected: %t\n", supportOnOff)
+	}
+	if tl.SupportsDim(id) != supportDim {
+		t.Fatalf("SupportsDim missmatch. Expected: %t\n", supportDim)
+	}
+	if tl.SupportsLearn(id) != supportLearn {
+		t.Fatalf("SupportsLearn missmatch. Expected: %t\n", supportLearn)
+	}
+	if tl.GetProtocol(id) != protocol {
+		t.Fatalf("Wrote protocol %s but %s is set\n", tl.GetProtocol(id), protocol)
+	}
+	if tl.GetModel(id) != model {
+		t.Fatalf("Wrote model %s but %s is set\n", tl.GetModel(id), model)
+	}
+	paramsSet := tl.GetParameters(id)
+	for key, val := range params {
+		if paramsSet[key] != val {
+			t.Fatalf("Parameter %s value was set to %s but value is %s\n", key, val, paramsSet[key])
+		}
+	}
+
+	// Remove device (cleanup after test)
 	err = tl.RemoveDevice(id)
 	if err != nil {
 		t.Fatalf("Failed to remove device %d. Reason: %s", id, err)
 	}
+}
+
+func TestCreateSelfLearnOnOffDevice(t *testing.T) {
+	params := make(map[string]string)
+	params["house"] = "1212131"
+	params["unit"] = "2"
+	createDeviceTest(t, "TestCreateSelfLearnOnOffDevice", "arctech",
+		"selflearning-switch:nexa", params, true, false, true)
+}
+
+func TestCreateFixedOnOffDevice(t *testing.T) {
+	params := make(map[string]string)
+	params["house"] = "1"
+	params["unit"] = "2"
+	createDeviceTest(t, "TestCreateFixedOnOffDevice", "risingsun",
+		"codeswitch", params, true, false, false)
 }
